316d38d6221bc987d1d45a24adaaac490a481145
Shuvo <smrakibulhasanshuvo@gmail.com>
2025-12-11 00:06:21 +0600

    day - 8 solve another one

---
+++ Day_8/A_Hit_the_Lottery.cpp
@@ -0,0 +1,31 @@
+// Problem link: https://codeforces.com/problemset/problem/996/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    long long bill = 0; 
+    while (n > 0)
+    {
+        if(n >= 100)
+            n -= 100;
+        else if(n >= 20)
+            n -= 20; 
+        else if(n >= 10)
+            n -= 10; 
+        else if(n >= 5)
+            n -= 5;
+        else if(n >= 1)
+            n -= 1;
+            
+        bill++; 
+    }
+    cout << bill << endl; 
+    
+    return 0;
+}
