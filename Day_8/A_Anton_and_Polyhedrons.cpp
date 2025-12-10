f513343dedd23967ec7ecbe537409efd2eec6c44
Shuvo <smrakibulhasanshuvo@gmail.com>
2025-12-10 23:49:33 +0600

    day - 8 update again

--- Day_8/A_Anton_and_Polyhedrons.cpp
+++ Day_8/A_Anton_and_Polyhedrons.cpp
@@ -14,16 +14,16 @@ int main()
         string s;
         cin >> s;
 
-        // if(s == "Tetrahedron")
-        //     count += 4; 
-        // else if(s == "Cube")
-        //     count += 6; 
-        // else if(s == "Octahedron")
-        //     count += 8;    
-        // else if(s == "Dodecahedron") 
-        //     count += 12; 
-        // else if(s == "Icosahedron")
-        //     count += 20; 
+        if(s == "Tetrahedron")
+            count += 4; 
+        else if(s == "Cube")
+            count += 6; 
+        else if(s == "Octahedron")
+            count += 8;    
+        else if(s == "Dodecahedron") 
+            count += 12; 
+        else if(s == "Icosahedron")
+            count += 20; 
     }
     cout << count << endl;
     return 0;
