9831cc9fe9c4d0592a2d6d78084ecc3368513d7d
Shuvo <smrakibulhasanshuvo@gmail.com>
2025-12-14 02:34:26 +0600

    day 10 one solve

---
+++ Day_10/A_Sum.cpp
@@ -0,0 +1,28 @@
+// Problem link: https://codeforces.com/problemset/problem/1742/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        vector<int> arr(3); 
+        for(int i = 0; i < 3; i++)
+        {
+            cin >> arr[i]; 
+        }
+
+        sort(arr.begin(), arr.end()); 
+
+        if(arr[0] + arr[1] == arr[2])
+            cout << "YES" << endl; 
+        else
+            cout << "NO" << endl;     
+    }
+    
+    return 0;
+}
