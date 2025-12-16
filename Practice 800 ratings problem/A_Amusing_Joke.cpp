2831e25c898672e7bdfd652647611733e03f3829
Shuvo <smrakibulhasanshuvo@gmail.com>
2025-12-17 02:03:39 +0600

    practice problem

---
+++ Practice 800 ratings problem/A_Amusing_Joke.cpp
@@ -0,0 +1,43 @@
+// Problem link: https://codeforces.com/problemset/problem/141/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string guest, host, mix_up; 
+
+    cin >> guest; 
+    cin >> host; 
+    cin >> mix_up; 
+
+    vector<char> store; 
+    for(int i = 0; i < guest.size(); i++)
+    {
+        store.push_back(guest[i]); 
+    }
+
+    for(int i = 0; i < host.size(); i++)
+    {
+        store.push_back(host[i]); 
+    }
+
+    vector<char> combine_store; 
+    for(auto &c : mix_up)
+    {
+        combine_store.push_back(c); 
+    }
+    sort(store.begin(), store.end()); 
+    sort(combine_store.begin(), combine_store.end()); 
+
+    if(store == combine_store
+    )
+    {
+        cout << "YES" << endl; 
+    }else
+    {
+        cout << "NO" << endl; 
+    }
+    return 0;
+}
